ae049cb7ca3d844928a8aa14fbb1873c4f1484bd
juan david cadena <jcbprofesion@gmail.com>
2025-05-24 16:39:45 -0500

    material del primer trimestre

---
+++ Variables (1).py
@@ -0,0 +1,35 @@
+#Las variables en python
+"""las variables son espacios de memoria donde se guardan y recuperan
+datos que utiliza el programa pudiendo asi guardar mucha informacion
+en una variable para asi poder llamar posteriormente esta informacion
+simplemente con poner el nombre de la variable un ejemplo de esto 
+podria ser"""
+
+saludo = "hola mundo"
+print (saludo)
+
+"""aqui podemos ver como de un nombre (saludo) y dentro de esta entre
+comillas nuestro texto y asi sollo llamamos a nuestra variable para 
+imprimirla y en caso tal se trate de una variable del tipo numerica 
+no se usan commillas"""
+
+multplicacion = 3
+print (multplicacion)
+
+"""ya en tal caso se desen realizar operaciones simplemente 
+se le crea una variable a cada valor y se crea otra variable en la 
+cual se opera directamente con las variable un ejemplo seria 
+12 x 17"""
+
+num1 = 12
+num2 = 17
+operacion = num1 * num2
+print (operacion)
+
+#condiciones para la creacion de una variable
+"""las variables deven de seguir una serie de reglas para ser creadas las cuales son 
+*No iniciar con un numero
+*No tener "-" 
+*no tener espacios
+*No llamarce como otra variable ya existente
+*las bariables diferencian entre letras mayusculas y minusculas"""
---
+++ clase1.py
@@ -0,0 +1,14 @@
+#tipos de datos en python
+"""los datos en python son de tres clases"""
+#datos escritos o letras 
+"""hola mundo,buenas tardes,saludos,etc"""
+#datos de tipo numerico
+"""estos datos tienen dos categorias las cuales son"""
+
+#numeros enteros(int)
+"""estos como su mismo nombre indica son numeros enteros
+digase 1,2,3,4,5,10,100,1000,etc"""
+
+#numeros flotantes o enteros(fload)
+"""con este termino se referencia a los numeros decimale como
+pueden ser 1,5 2,5 3,5 etc"""
